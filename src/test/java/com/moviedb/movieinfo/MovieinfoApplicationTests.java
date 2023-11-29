0f3dbbb40e5aa1bd7b03049319fba94d01639366
tahereh rasaei <taherehrasaei@gmail.com>
2023-11-29 08:03:14 +0330

    initial commit

---
+++ src/test/java/com/moviedb/movieinfo/MovieinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviedb.movieinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
